8034c9defb8e5e0d941ebadc62dbe475e4bb37a7
Ali-johnno <ajay051701@gmail.com>
2021-02-23 01:20:26 -0500

    Exercise 1

--- app/config.py
+++ app/config.py
@@ -1,12 +1,17 @@
 import os
+from flask import Flask
 
 class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME') or 'admin'
-    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD') or 'Password123'
+    USERNAME = os.environ.get('ADMIN_USERNAME') or 'admin'
+    PASSWORD = os.environ.get('ADMIN_PASSWORD') or 'Password123'
+    UPLOAD_FOLDER = './uploads'
 
+    app = Flask(__name__)
+
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
